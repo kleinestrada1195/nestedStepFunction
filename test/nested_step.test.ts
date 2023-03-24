7ecacc1898d76227bc026a6a3a99919f61c7b430
Klein Estrada <91865551+kleinestrada1195@users.noreply.github.com>
2023-03-24 11:30:57 +1100

    Initial commit

---
+++ test/nested_step.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NestedStep from '../lib/nested_step-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nested_step-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NestedStep.NestedStepStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
